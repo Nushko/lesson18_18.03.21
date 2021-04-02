93aa21d9f1462816c0abb67afaac36480b7e25b4
Nushko <anushpallaev@gmail.com>
2021-04-02 21:12:07 +0300

    18ое дз

---
+++ lesson18_18.03.21/DudesTable.cs
@@ -0,0 +1,93 @@
+﻿using System.Linq;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace lesson18_18._03._21
+{
+    class DudesTable
+    {
+        private const string ConString = @"Server=NUSHKOPC\SQLSERVER; Initial Catalog=AlifBase; Integrated Security=True";
+
+        public class Dude
+        {
+            public int Id { get; set; }
+            public string Name { get; set; }
+            public int Age { get; set; }
+            public string Surname { get; set; }
+        }
+
+        public class Commands
+        {
+            public static void Create(Dude input)
+            {
+                try
+                {
+                    using (var conn = new SqlConnection(ConString))
+                    {
+                        var sql =
+                            @"insert into Dudes (Name, Age, Surname) 
+                        values (@Name, @Age, @Surname)";
+                        conn.Execute(sql, input);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine("Create error - " + ex);
+                }
+            }
+            public static Dude Read(int id)
+            {
+                try
+                {
+                    using (var conn = new SqlConnection(ConString))
+                    {
+                        return conn.Query<Dude>(@"select * from Dudes where Id = @id").First();
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine("Read error - " + ex);
+                }
+                return default;
+            }
+
+            public static void Update(Dude input)
+            {
+                try
+                {
+                    using (var conn = new SqlConnection(ConString))
+                    {
+                        var sql =
+                            "update Dudes set " +
+                            "Name = @Name, " +
+                            "Age = @Age, " +
+                            "Surname = @Surname, " +
+                            "WHERE Id = @Id";
+                        conn.Execute(sql, input);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine("Update error - " + ex);
+                }
+            }
+
+            public static void Delete(int id)
+            {
+                try
+                {
+                    using (var conn = new SqlConnection(ConString))
+                    {
+                        var sql =
+                            $"delete from Dudes where Id = @id";
+                        conn.Execute(sql, id);
+                    }
+                }
+                catch (System.Exception ex)
+                {
+                    System.Console.WriteLine("Delete error - " + ex);
+                }
+            }
+        }
+    }
+}
---
+++ lesson18_18.03.21/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Dapper;
+using Microsoft.Data.SqlClient;
+
+namespace lesson18_18._03._21
+{
+    //sql сервер перестал работать, пришлось переустанавливать
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var test = new DudesTable.Dude
+            {
+                Name = "Anush",
+                Age = 18,
+                Surname = "Pall"
+            };
+            DudesTable.Commands.Create(test);
+        }
+    }
+}
